56cef80eace3e36cade0446cf3fb6faa7b5b7a7b
Manuch Sadri <colonquebang@hotmail.com>
2023-03-31 23:20:22 +0000

    completed parse_template() with passing unit tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -6,7 +6,7 @@ test_template = "assets/dark_and_stormy_night_template.txt"
 
 def main():
     welcome()
-    parse_template("It was a {Adjective} and {Adjective} {Noun}.")
+    #parse_template(tc)
 
 def welcome(response = str()):
     print(textwrap.dedent("""
@@ -26,29 +26,43 @@ def welcome(response = str()):
             return "Please type RUN or QUIT."
         response = input ("> ")
         response = response.lower()
-        # if response:
-        #     print("\nPlease type RUN or QUIT.")
-        #     return "Please type RUN or QUIT."
+        if response == "run":
+            #print("run")
+            read_template()
+        elif response == "quit":
+            #print("break")
+            break
     return response
 
-def read_template(source = template):
+def read_template(source = test_template):
+# def read_template(source = template):
     # tf, tc = template file, template contents
+    #print("read_template")
+    # try to open madlibs file source, read contents, and send contents to parse_template()
     try:
         with open(source, "r") as tf:
             tc = tf.read()
+            parse_template(tc)
             return tc
+    # if madlibs file source is not found return error message
     except FileNotFoundError:
         return "The template file was not found."
 
 def parse_template(string):
-    stripped = re.sub(r'\{.+?}', '', string)
-    print(stripped)
-    return stripped
+    # set regex pattern to match madlibs blanks
+    pattern = r'\{([^}]*)\}'
+    # set parts = a tuple containing the madlibs blanks from the string
+    parts = tuple(re.findall(pattern, string))
+    # set stripped = source string with the madlibs blanks removed replacing the regex matches with empty {}
+    stripped = re.sub(pattern, "{}", string)
+    #print(parts)
+    #print(stripped)
+    # send stripped and parts to the merge()
+    merge(stripped, parts)
+    return stripped, parts
 
-
-
-def merge():
+def merge(stripped, matches):
     pass
 
 
-#main()
+main()
