85d7d0c5136d5838d55922f68d25445ad0d6b4f2
Manuch Sadri <colonquebang@hotmail.com>
2023-04-01 02:17:50 +0000

    added file output, but assocaited unit test is failing

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,17 +1,19 @@
 import textwrap
 import re
 
-template = "assets/make_me_a_video_game_template.txt"
-test_template = "assets/dark_and_stormy_night_template.txt"
+# template = "assets/make_me_a_video_game_template.txt"
+# test_template = "assets/dark_and_stormy_night_template.txt"
 
 def main():
+    #template = "assets/make_me_a_video_game_template.txt"
+    template = "assets/dark_and_stormy_night_template.txt"
     welcome()
-    ml_file = read_template()
+    ml_file = read_template(template)
     if ml_file != "The template file was not found.":
         ml_stripped, ml_parts = parse_template(ml_file)
         ml_responses = fill_in_the_blank(ml_parts)
         ml_assembled = merge(ml_stripped, ml_responses)
-        write_file(ml_assembled)
+        write_file(template, ml_assembled)
     else:
         print("end")
 
@@ -26,8 +28,9 @@ def welcome(response = str()):
         **************************************************************************
         """))
 
-def read_template(source = test_template):
+#def read_template(source = test_template):
 #def read_template(source = template):
+def read_template(source):
     # tf, tc = template file, template contents
     # try to open madlibs file source, read contents, and send contents to parse_template()
     try:
@@ -37,6 +40,8 @@ def read_template(source = test_template):
     # if madlibs file source is not found return error message
     except FileNotFoundError:
         print("The template file was not found.")
+        with open("tests/missing.txt", "w") as output:
+            output.write("The template file was not found.")
         return "The template file was not found."
 
 def parse_template(string):
@@ -62,8 +67,12 @@ def merge(story, responses):
     print(story)
     return story
 
-def write_file(output):
-    #with open(, 'w') as
-    pass
+def write_file(loc, finished):
+    print(loc)
+    file_name = loc.replace("template", "output")
+    print(file_name)
+    with open(file_name, "w") as output:
+        output.write(finished)
+
 
 main()
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,26 +1,26 @@
 import pytest
 from madlib_cli.madlib import read_template, parse_template, merge
 
-@pytest.mark.skip("pending")
+#@pytest.mark.skip("pending")
 def test_read_template_returns_stripped_string():
     actual = read_template("assets/dark_and_stormy_night_template.txt")
     expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
-@pytest.mark.skip("pending")
+#@pytest.mark.skip("pending")
 def test_read_template_returns_stripped_string2():
     actual = read_template("assets/make_me_a_video_game_template.txt")
     expected = """Make Me A Video Game!\n\nI the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} {A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!\n\nWhat are a {Large Animal} and backpacking {Small Animal} to do? Before you can help {A Girl's Name}, you'll have to collect the {Adjective} {Plural Noun} and {Adjective} {Plural Noun} that open up the {Number 1-50} worlds connected to A {First Name}'s Lair. There are {Number} {Plural Noun} and {Number} {Plural Noun} in the game, along with hundreds of other goodies for you to find."""
     assert actual == expected
 
-@pytest.mark.skip("pending")
+#@pytest.mark.skip("pending")
 def test_read_template_returns_stripped_string_error():
     actual = read_template("assets/make_me_a_vide0_game_template.txt")
     expected = "The template file was not found."
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+#@pytest.mark.skip("pending")
 def test_parse_template():
     actual_stripped, actual_parts = parse_template(
         "It was a {Adjective} and {Adjective} {Noun}."
@@ -39,7 +39,7 @@ def test_merge():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+#@pytest.mark.skip("pending")
 def test_read_template_raises_exception_with_bad_path():
 
     with pytest.raises(FileNotFoundError):
